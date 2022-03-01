22e8e550faf6046c227d4452103dcf6a095e3b94
lupinxian64 <100187259+lupinxian64@users.noreply.github.com>
2022-03-01 15:37:14 +0800

    Update repeater.c

--- repeater.c
+++ repeater.c
@@ -1,41 +1,14 @@
-/*
- * the following code snippet is modified from this website
- * https://www.cplusplus.com/reference/cstdlib/rand/
- **/
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 
-int main () {
+int main() {
 
-    //renew the seed of rand function
-    srand(time(NULL));
+    int input;
 
-    //get the answer between 0 to 99
-    int answer = rand() % 100;
-
-    //initialize
-    int guess = -1;
-    int counter = 0;
-
-    //prompt message
-    printf("0到99，猜一個數字。");
-
-    //get input
-    //continue if the input is integer
-    while (scanf("%d", &guess)) {
-        if (guess == answer) { //if you have got the answer
-            printf("你猜的是︰%d，恭喜猜中了。一共猜了%d次。\n", guess, counter);
-            return 0;
-        } else { //go on if not correct
-            counter++;
-            printf("你猜的是︰%d，", guess);
-            if (guess > answer) {
-                printf("低一點。");
-            } else {
-                printf("高一點。");
-            }
-        }
+    printf("每輸入一個數字，程式會再印出相同的數字。\n");
+    //fill some condition into the following while loop
+    //only ONE line code is you need
+    //please refer page 4 of homework request
+    while (scanf("%d" , &input)) {
+        printf("%d\n", input);
     }
-    printf("你輸入的不是數字。");
 }
