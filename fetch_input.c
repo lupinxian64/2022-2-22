3884dc3b6c021cbddcd0f2bb67a3389596a87401
lupinxian64 <100187259+lupinxian64@users.noreply.github.com>
2022-03-01 15:32:18 +0800

    Create fetch_input.c

---
+++ fetch_input.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+
+    int a ;
+    int b ;
+    int c ;
+    int d ;
+    int e ;
+
+    printf("請輸入五個數字，以空白隔開。\n");
+    //add scanf code to get data from input
+    //and feed those input into a, b, c, d, e, respectively.
+    //only ONE line code is you need
+    //please refer page 18 of slides
+    scanf("%d %d %d %d %d", &a, &b, &c, &d, &e);
+
+    printf("a=%d\n", a);
+    printf("b=%d\n", b);
+    printf("c=%d\n", c);
+    printf("d=%d\n", d);
+    printf("e=%d\n", e);
+}
+
